1a9bc6f569cb5ce286355d4e1234bbe45dc9db57
GnuTLam <131742384+GnuTLam@users.noreply.github.com>
2024-01-18 10:37:16 +0700

    MainFrame.cpp
    báo cáo

--- MainFrame.cpp
+++ MainFrame.cpp
@@ -1,19 +1,19 @@
-﻿#include "MainFrame.h"
+#include "MainFrame.h"
 #include "Sort.h"
 #include <wx/wx.h>
 #include <wx/spinctrl.h>
-
+double unit;
+// Hàm khởi tạo các đối tượng cột (panel) với các tham số là chiều cao, chiều rộng, khoảng cách, số lượng.
 void align_frame(std::vector<int>& cc, std::vector<int>& cr, std::vector<int>& kc, int value) {
     cc.resize(value + 1);
     cr.resize(value + 1);
     kc.resize(value + 1);
+
     for (int i = 1; i <= value; i++) cc[i] = i;
-    //tao cc cach random
     for (int i = 1; i <= value; i++) {
         std::swap(cc[i], cc[rand() % value + 1]);
     }
 
-    //tao khoang cach
     int s_frame = 760 / value;
     std::vector<int> frame(value + 1, s_frame);
     int tmp = 760 - s_frame * value;
@@ -51,9 +51,9 @@ void align_frame(std::vector<int>& cc, std::vector<int>& cr, std::vector<int>& k
         std::swap(cr[i], cr[k]);
         std::swap(kc[i], kc[k]);
     }
-
 }
 
+// Constructor MainFrame với tham số là tiêu đề của khung chính.
 MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) {
     panel = new wxPanel(this);
     panel->SetBackgroundColour(wxColour(49, 48, 77));
@@ -65,17 +65,17 @@ MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) 
     panelsub3->SetBackgroundColour(wxColour(240, 236, 229));
   
     //Option
-    //1.
+    //1. Tiêu đề "Option"
     wxStaticText* textOption = new wxStaticText(panelsub2, wxID_ANY, "OPTIONS", wxPoint(85, 10));
     textOption->SetFont(wxFont(16, wxFONTFAMILY_DECORATIVE, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD));
 
     wxPanel* panelsubsub1 = new wxPanel(panelsub2, wxID_ANY, wxPoint(0, 42), wxSize(260, 3));
     panelsubsub1->SetBackgroundColour(wxColour(49, 48, 77));
 
-    //2.Generate column
+    //2.Tạo các cột
     wxStaticText* textColumn = new wxStaticText(panelsub2, wxID_ANY, "Column:", wxPoint(25, 85));
     textColumn->SetFont(wxFont(11, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD));
-    spinCol = new wxSpinCtrl(panelsub2, wxID_ANY, "10 - 50", wxPoint(89, 80), wxSize(94, 24), wxSP_ARROW_KEYS, 10, 50);
+    spinCol = new wxSpinCtrl(panelsub2, wxID_ANY, "10", wxPoint(89, 80), wxSize(94, 24), wxSP_ARROW_KEYS, 5, 20,8);
     spinCol->SetFont(wxFont(12, wxFONTFAMILY_DECORATIVE, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL));
 
 
@@ -83,7 +83,7 @@ MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) 
     button->SetFont(wxFont(10, wxFONTFAMILY_DECORATIVE, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD));
     button->Bind(wxEVT_BUTTON, &MainFrame::ButtonCreat, this);
 
-    //3. Sorting type
+    //3. Lựa chọn phương thức sắp xếp
     wxStaticText* textSort = new wxStaticText(panelsub2, wxID_ANY, "Sorting:", wxPoint(24, 150));
     textSort->SetFont(wxFont(11, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD));
     wxArrayString choices;  choices.Add("    Selection Sort");  choices.Add("    Bubble Sort"); choices.Add("    Insertion Sort");  choices.Add("    Merge Sort"); choices.Add("    Quick Sort");
@@ -92,17 +92,19 @@ MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) 
     choice->Select(0);
 
 
-    //Start & Ttin sort
+    //4. Thực hiện sắp xếp
     wxButton* buttonstart = new wxButton(panelsub3, wxID_ANY, "Start", wxPoint(100, 20), wxSize(60, 30));
     buttonstart->SetFont(wxFont(10, wxFONTFAMILY_DECORATIVE, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD));
     buttonstart->Bind(wxEVT_BUTTON, &MainFrame::ButtonStart, this);
 
 }
 
+// Constructor ButtonCreate với tham số là sự kiện ấn nút Create.
+// Sinh ra các cột(panel) theo số lượng mong muốn.
 void MainFrame::ButtonCreat(wxCommandEvent& event) {
     int value = spinCol->GetValue();
-
     int x = 20, y;
+    unit = 650.0 / value * 0.82;
 
     // Xóa tất cả các panel đã tạo trước đó
     for (wxPanel* panel : colpanel) {
@@ -110,28 +112,31 @@ void MainFrame::ButtonCreat(wxCommandEvent& event) {
     }
     colpanel.clear();
 
-    double unit = 650.0 / value*0.82;
+    // Sinh ra các cột(panel)
     std::vector<int> cc, cr, kc;
     align_frame(cc, cr, kc, value);
     for (int i = 1; i <= value; i++)
     {
         int h = round(cc[i] * unit);
         y = 570 - h;
+
         wxPanel* panelcol = new wxPanel(panelsub1, wxID_ANY, wxPoint(x, y), wxSize(cr[i], h));
         panelcol->SetBackgroundColour(wxColour(22, 26, 48));
         x = x + cr[i] + kc[i];
 
-        // Lưu trữ con trỏ đến panel đã tạo
         colpanel.push_back(panelcol);
         panelsub1->Refresh();
     }
-
 }
 
+
+// Constructor ButtonCreate với tham số là sự kiện ấn nút Start.
+// Thực hiện công việc sắp xếp.
 void MainFrame::ButtonStart(wxCommandEvent& event)
 {
     Sort sortpanel;
     sortpanel.SetPanelSub1(panelsub1);
+    sortpanel.SetUnit(unit);
     wxString select=choice->GetStringSelection();
     int index = choice->GetSelection();
     
@@ -148,8 +153,9 @@ void MainFrame::ButtonStart(wxCommandEvent& event)
         sortpanel.Merge_Sort(colpanel,0, spinCol->GetValue()-1);
     }
     else if (index == 4) {
-        std::cout << "1234";
         sortpanel.Quick_Sort(colpanel, 0, spinCol->GetValue() - 1);
     }
-    wxLogMessage(" %s (Index: %d)", select, index);
+
+    // Thông báo hoàn tất việc sort
+    wxLogMessage(" DONE \n %s (Index: %d)", select, index);
 }
